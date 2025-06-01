782f8de90ba5dc032b93dd9cd883f9e52ad80831
nikitatkacenro <nikita.junior1234fanger22.user@gmail.com>
2025-06-01 11:06:31 +0300

    commit message12

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 
 
 
